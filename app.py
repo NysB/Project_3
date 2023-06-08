a0e4feaaf59a49b016f007d115dd0caf1a24194c
NysB <123317735+NysB@users.noreply.github.com>
2023-06-08 20:53:11 +0100

    Update Data Retrieval

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # Import the dependencies.
 import numpy as np
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
+import pandas as pd
+
+from flask import Flask, jsonify, render_template
 
 
 #################################################
@@ -12,21 +15,16 @@ from flask import Flask, jsonify
 #################################################
 engine = create_engine("sqlite:///NBA_stats.sqlite")
 
-# print(base.classes.keys())
-
 # reflect an existing database into a new model
-base = automap_base()
+Base = automap_base()
 # reflect the tables
-base.prepare(engine, reflect=True)
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-Team_table= base.classes.Team_table
-Player_score_table = base.classes.Player_score_table
-Player_info_table = base.classes.Player_info_table
+Team_table= Base.classes.Team_table
+Player_score_table = Base.classes.Player_score_table
+Player_info_table = Base.classes.Player_info_table
 
-
-# Create our session (link) from Python to the DB
-session = Session(engine)
 
 
 #################################################
@@ -41,52 +39,81 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def home():
+    # below will need to be replaced by html
     return "Welcome to the NBA Stats API!"
+    # return render_template('index.html')
 
 
 # teams route
 @app.route("/teams")
 def get_teams():
     # Query the team table and return the results as JSON
+
+    session = Session(engine)
     teams = session.query(Team_table.Team, Team_table.PPG, Team_table.APG, Team_table.RPG).all()
+    session.close()
+    
     results = []
-    for team in teams:
-        results.append({
-            "team": team.Team,
-            "PPG": team.PPG,
-            "APG": team.APG,
-            "RPG": team.RPG
-        })
+    for Team, PPG, APG, RPG in teams:
+        team_dict = {}
+        team_dict["Team"] = Team
+        team_dict["PPG"] = PPG
+        team_dict["APG"] = APG
+        team_dict["RPG"] = RPG
+        results.append(team_dict)
+    
     return jsonify(results)
+
 
 #  player scores route
 @app.route("/player_scores")
 def get_player_scores():
     # Query the Player_score_table and return the results as JSON
-    player_scores = session.query(Player_score_table.Player, Player_score_table.RPG, Player_score_table.APG, Player_score_table.PPG).all()
+    
+    session = Session(engine)
+    player_scores = session.query(Player_score_table.Player, Player_score_table.Year, Player_score_table.GP, Player_score_table.GS).all()
+    session.close()
+
     results = []
-    for player_score in player_scores:
-        results.append({
-            "player": player_score.Player,
-            "RPG": player_score.RPG,
-            "APG": player_score.APG,
-            "PPG": player_score.PPG
-        })
+    for Player, Year, GP, GS in player_scores:
+        player_score_dict = {}
+        player_score_dict["Player"] = Player
+        player_score_dict["Year"] = Year
+        player_score_dict["GP"] = GP
+        player_score_dict["GS"] = GS
+        results.append(player_score_dict)
+    
     return jsonify(results)
+
 
 # player info route
 
 @app.route("/player_info")
 def get_player_info():
     # Query the player info table and return the results as JSON
-    player_info = session.query(Player_info_table).all()
+    
+    session = Session(engine)
+    player_info = session.query(Player_info_table.Player, 
+                                Player_info_table.Current_team, 
+                                Player_info_table.Age, 
+                                Player_info_table.Height,
+                                Player_info_table.Weight,
+                                Player_info_table.NBA_status,
+                                Player_info_table.Draft_entry).all()
+    session.close()
+
     results = []
-    for info in player_info:
-        results.append({
-            "name": info.name,
-            "current_team": info.current_team,
-            "age": info.age
-        })
+    for Player, Current_team, Age, Height, Weight, NBA_status, Draft_entry in player_info:
+        player_info_dict = {}
+        player_info_dict["Player"] = Player
+        player_info_dict["Current_team"] = Current_team
+        player_info_dict["Age"] = Age
+        player_info_dict["Height"] = Height
+        player_info_dict["Weight"] = Weight
+        player_info_dict["NBA Status"] = NBA_status
+        player_info_dict["Draft Entry"] = Draft_entry
+        results.append(player_info_dict)
+    
     return jsonify(results)
 
 
---
+++ plots.js
@@ -0,0 +1,37 @@
+// Grab data
+let apiTitanic = "/api/v1.0/passengersbyclass";
+
+function init() {
+  d3.json(apiTitanic).then(function(data) {
+      let filter = "Survived"
+      data = data['Survived']
+      let dataPlot = [{
+        x: Object.keys(data),
+        y: Object.values(data),
+        type: 'bar'
+      }]
+      Plotly.newPlot("plot", dataPlot);
+    });
+  }
+
+// Function called by DOM changes
+function refreshPlot() {
+  let dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a letiable
+  let filter = dropdownMenu.property("value");
+  // Call function to update the chart
+  d3.json(apiTitanic).then(function(data) {
+    data = data[filter]
+    let dataPlot = [{
+      x: Object.keys(data),
+      y: Object.values(data),
+      type: 'bar'
+    }]
+    Plotly.newPlot("plot", dataPlot);
+  });
+}
+
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", refreshPlot);
+
+init();
