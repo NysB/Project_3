b23b10c1d516080c1b608436832c9ea76587fdbc
xc1614 <124749040+xc1614@users.noreply.github.com>
2023-06-08 22:01:02 -0400

    update

--- plots.js
+++ plots.js
@@ -35,7 +35,100 @@
 // d3.selectAll("#selDataset").on("change", refreshPlot);
 
 // init();
-// API endpoint for team data
+
+
+
+
+
+
+
+// Pie Chart
+
+fetch("http://127.0.0.1:5000/player_scores")
+  .then(response => response.json())
+  .then(data => {
+    // Process the retrieved JSON data
+    console.log(data);
+
+    // Update your HTML elements with the data
+    let container = document.getElementById("container");
+
+    // Create HTML elements and populate them with the data
+    data.forEach(player => {
+      let playerElement = document.createElement("div");
+      playerElement.textContent = `${player.Player}: ${player.GP} GP, ${player.GS} GS`;
+
+      container.appendChild(playerElement);
+    });
+  })
+  .catch(error => {
+    // Handle any errors
+    console.log(error);
+  });
+
+let playerInfoApi = "http://127.0.0.1:5000/player_scores";
+
+function init() {
+  // Fetch player info and scores from the API
+  d3.json(playerInfoApi).then(function(data) {
+    let dropdownMenu = d3.select("#selDataset");
+
+    // Populate the dropdown menu with player names
+    data.forEach(function(player) {
+      dropdownMenu.append("option")
+        .attr("value", player.Player)
+        .text(player.Player);
+    });
+
+    // Initialize the chart with the first player
+    let initialPlayer = data[0].Player;
+    updatePlot(initialPlayer);
+  });
+}
+
+// Function to update the pie chart
+function updatePlot(player) {
+  // Fetch player scores for 2022 from the API
+  let scoresApi = `http://localhost:5000/player_scores?player=${player}&year=2022`;
+  d3.json(scoresApi).then(function(scores) {
+    let labels = ["GP", "GS", "AST", "PTS", "TRB"];
+    let values = [scores.GP, scores.GS, scores.AST, scores.PTS, scores.TRB];
+
+    let dataPlot = [{
+      labels: labels,
+      values: values,
+      type: 'pie'
+    }];
+
+    let layout = {
+      title: `Player Scores for ${player} (2022)`
+    };
+
+    // Update the chart
+    Plotly.newPlot("plot", dataPlot, layout);
+  });
+}
+
+// Function called by DOM changes
+function refreshPlot() {
+  let dropdownMenu = d3.select("#selDataset");
+  let selectedPlayer = dropdownMenu.property("value");
+  updatePlot(selectedPlayer);
+}
+
+// Initialize the page
+init();
+
+// On change to the dropdown menu, call refreshPlot()
+d3.selectAll("#selDataset").on("change", refreshPlot);
+
+
+
+
+
+
+// Line Chart
+
 let teamDataApi = "http://127.0.0.1:5000/teams";
 
 function init() {
