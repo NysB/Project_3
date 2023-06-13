6530ee7863856eb9bf26787acdf5f3e3c4a99a7a
xc1614 <124749040+xc1614@users.noreply.github.com>
2023-06-13 12:21:09 -0400

    5

--- static/js/plots.js
+++ static/js/plots.js
@@ -97,7 +97,26 @@ function getPlayerInfo() {
 //   };
   
 //   Plotly.newPlot("teamLineChart", dataPlot, layout);
-// }
+// }function updateTeamBarChart(team, teams) {
+  let filteredTeam = teams.find((t) => t.Team === team);
+  let teamCategories = ["APG", "PPG", "RPG"];
+  let teamScores = teamCategories.map((category) => {
+    return filteredTeam[category];
+  });
+
+  let dataPlot = [
+    { x: teamCategories, y: teamScores, type: "bar" }
+  ];
+
+  let layout = {
+    title: "Team Scores",
+    xaxis: { title: "Category" },
+    yaxis: { title: "Scores" }
+  };
+
+  Plotly.newPlot("teamBarChart", dataPlot, layout);
+
+
 
 function populateDropdown(teams) {
   let dropdownMenu = d3.select("#selDataset");
