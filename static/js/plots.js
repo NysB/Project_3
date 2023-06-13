70c4d5c4f778af736307d1ac0b3de60e7aa380d8
xc1614 <124749040+xc1614@users.noreply.github.com>
2023-06-13 14:41:54 -0400

    7

--- static/js/plots.js
+++ static/js/plots.js
@@ -98,16 +98,6 @@ function getPlayerInfo() {
   
 //   Plotly.newPlot("teamLineChart", dataPlot, layout);
 // }
-
-function populateDropdown(teams) {
-  let dropdownMenu = d3.select("#selDataset");
-  dropdownMenu.selectAll("option")
-    .data(teams)
-    .enter()
-    .append("option")
-    .text((team) => team.Team)
-    .attr("value", (team) => team.Team)
-    .property("selected", (team, i) => i === 0);
   
   dropdownMenu.on("change", () => {
     const selectedTeam = d3.select("#selDataset").node().value;
@@ -119,7 +109,6 @@ function populateDropdown(teams) {
         console.log(error);
       });
   });
-}
 
 function updateCharts(team, playerInfo) {
   updateTeamBarChart(team);
